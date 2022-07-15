58b8d121d99f0332a13fe0c66efd38cc664613b8
FedeNB <federiconb262@gmail.com>
2022-07-14 23:00:08 -0300

    implementacion de herencia y configuracion de admin

--- mi_app/urls.py
+++ mi_app/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from mi_app.views import saludar_a, listar_cursos, listar_estudiantes, mostrar_index
 
 
+
 urlpatterns = [
     path('mi-pagina/', mostrar_index),
     path('saludar/persona/<nombre>', saludar_a),
     path('listar-cursos/', listar_cursos),
     path('listar-estudiantes/', listar_estudiantes),
+
 ]
